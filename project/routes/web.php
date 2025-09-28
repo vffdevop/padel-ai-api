55fef0265d3576c57d5720fbb1bd1cb6ffe35a64
vffdevop <vffdevop@gmail.com>
2025-09-28 13:12:59 +0200

    verify: Docker setup wortking, laravel on port 8080. Change in nginx\defailt.conf because public folder

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/health', function () {
+    return '{"status" : "ok"}';
+});
+
+Route::get('/health2', function () {
+    return response()->json([
+            'status' => 'ok'
+        ], Response::HTTP_OK);
 });
