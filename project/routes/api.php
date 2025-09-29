bdf258b185ecc210169701a3f731631405e50745
vffdevop <vffdevop@gmail.com>
2025-09-29 10:43:57 +0200

    fix: update routes/api

--- project/routes/api.php
+++ project/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TaskController;
 use Symfony\Component\HttpFoundation\Response;
 
 Route::apiResource('tasks', TaskController::class);
